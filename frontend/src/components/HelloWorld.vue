00fd49c4c7b2b0a8fd024aba2fc8b7e700a422a5
hoang <vinhyenvodoi98@gmail.com>
2020-05-03 16:37:17 +0700

    feat : use socket.io-client instead of vue-socket.io

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,14 +1,85 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <v-flex md8 offset-md2 class="pa-1" @submit.prevent="sendName">
+      <v-form style="width:100%">
+        <v-text-field
+          label="Solo"
+          placeholder="Type Your Name"
+          v-model="name"
+          solo
+          ref="name"
+        ></v-text-field>
+      </v-form>
+    </v-flex>
+
+    <v-flex md8 offset-md2 class="pa-1" @submit.prevent="sendMessage">
+      <v-form style="width:100%">
+        <v-text-field
+          label="Solo"
+          placeholder="Type Your Message"
+          v-model="message"
+          solo
+          ref="message"
+        >
+        </v-text-field>
+      </v-form>
+    </v-flex>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import io from 'socket.io-client';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      name: '',
+      message: '',
+      socket: io('localhost:3000'),
+    };
+  },
+
+  mounted: function() {
+    this.socket.on('changeData', (data) => {
+      console.log('data', data);
+    });
+  },
+  methods: {
+    // with http
+    async sendName() {
+      if (this.name) {
+        let name = {
+          name: this.name,
+        };
+        axios
+          .post('http://localhost:3000/update', name)
+          .then((res) => {
+            console.log('res', res);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        this.name = '';
+      }
+    },
+
+    // with socket
+    async sendMessage() {
+      if (this.message) {
+        let message = {
+          handle: this.handle,
+          message: this.message,
+        };
+        await this.socket.emit('chat', message);
+        this.message = '';
+      }
+    },
   },
 };
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,24 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
-import VueSocketIO from 'vue-socket.io';
-// import SocketIO from 'socket.io-client';
-
-Vue.use(
-  new VueSocketIO({
-    debug: true,
-    connection: 'http://localhost:3000/events',
-    vuex: {
-      store,
-      actionPrefix: 'SOCKET_',
-      mutationPrefix: 'SOCKET_',
-    },
-  })
-);
+import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   store,
   render: (h) => h(App),
 }).$mount('#app');
